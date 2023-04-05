03fc6d8f79eb19f3453cb3bdc277c6716e3b7b88
HoangNguyen <xuanhoang.nguyen@akselos.com>
2023-04-05 16:37:03 +0700

    add stats and dat gui

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
+
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(1))
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -35,6 +39,34 @@ function onWindowResize() {
     render()
 }
 
+// Stats
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+//dat GUI
+const gui = new GUI()
+const cubeFolder = gui.addFolder('Cube')
+const cubeRotationFolder = cubeFolder.addFolder('Rotation')
+cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+cubeRotationFolder.open()
+const cubePositionFolder = cubeFolder.addFolder('Position')
+cubePositionFolder.add(cube.position, 'x', -10, 10, 2)
+cubePositionFolder.add(cube.position, 'y', -10, 10, 2)
+cubePositionFolder.add(cube.position, 'z', -10, 10, 2)
+cubeFolder.open()
+cubePositionFolder.open()
+const cubeScaleFolder = cubeFolder.addFolder('Scale')
+cubeScaleFolder.add(cube.scale, 'x', -5, 5)
+cubeScaleFolder.add(cube.scale, 'y', -5, 5)
+cubeScaleFolder.add(cube.scale, 'z', -5, 5)
+cubeFolder.add(cube, 'visible')
+cubeFolder.open()
+cubeScaleFolder.open()
+
+
 function animate() {
     requestAnimationFrame(animate)
 
@@ -42,11 +74,12 @@ function animate() {
     // cube.rotation.y += 0.01
 
     render()
+    stats.update()
 }
 
 function render() {
     renderer.render(scene, camera)
 }
 
-// animate()
-render()
+animate()
+// render()
